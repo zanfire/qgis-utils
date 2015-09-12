8724e58406a2ca9aa9ecb657c5571363f468c930
Matteo Valdina <mvaldina@MacBook-Pro.local>
2015-09-12 19:05:29 +0200

    Refined Assign class dialog to the real input.

--- gjko-plugin/dialogs/AssignClassDialog.py
+++ gjko-plugin/dialogs/AssignClassDialog.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore, QtGui 
 from qgis.utils import iface
 from Ui_AssignClass import Ui_Dialog
+import os
 
 class AssignClassDialog(QtGui.QDialog):
 
@@ -11,8 +12,9 @@ class AssignClassDialog(QtGui.QDialog):
         layers = iface.legendInterface().layers()
         for l in layers:
             self.ui.energyCombo.addItem(l.name())
-            self.ui.certificateCombo.addItem(l.name())
-            self.ui.istatCombo.addItem(l.name())
+            self.ui.istatCombo.addItem(l.name())   
+        QtCore.QObject.connect(self.ui.epcOpenButton, QtCore.SIGNAL('clicked()'), self.epcOpenFile)
+        QtCore.QObject.connect(self.ui.istatOpenButton, QtCore.SIGNAL('clicked()'), self.istatOpenFile)
     
     def energy_layer_name(self):
        return str(self.ui.energyCombo.currentText())
@@ -25,4 +27,16 @@ class AssignClassDialog(QtGui.QDialog):
 
     #def create_intersection_layer_check(self):
     #     return self.ui.intersectionLayerCheckBox.isChecked()
+
+    def epcOpenFile(self):
+        f = self.openFile()
+        self.ui.epcEdit.setText(f)
+
+    def istatOpenFile(self):
+        f = self.openFile()
+        self.ui.istatEdit.setText(f)
+    
+    def openFile(self):
+        location = QtGui.QFileDialog.getOpenFileName(None, 'Open CSV file:', os.getenv('HOME'), 'CSV (*.csv);; All files (*)')
+        return location
 
---
+++ gjko-plugin/logic/agecompactness.py
@@ -0,0 +1,57 @@
+"""
+
+ctess ratio             construction age               
+ <0.3    1       <=1945  A          
+0.3-0.4    2       1946-1960   B          
+0.4-0.5   3       1961-1980   C          
+0.5-0.6  4       1981-1990   D          
+0.6-0.7     5       1991-2005   E          
+0.7-0.8    6       >=2006  F          
+0.8     7                      
+
+For residential buildings                           
+<0.3    0.3-0.4     0.4-0.5     0.5-0.6     0.6-0.7     0.7-0.8     >=0.8
+<=1945     E1-A.1  E1-A.2  E1-A.3  E1-A.4  E1-A.5  E1-A.6  E1-A.7
+1946-1960     E1-B.1  E1-B.2  E1-B.3  E1-B.4  E1-B.5  E1-B.6  E1-B.7
+1961-1980    E1-C.1  E1-C.2  E1-C.3  E1-C.4  E1-C.5  E1-C.6  E1-C.7
+1981-1990   E1-D.1  E1-D.2  E1-D.3  E1-D.4  E1-D.5  E1-D.6  E1-D.7
+1991-2005  E1-E.1  E1-E.2  E1-E.3  E1-E.4  E1-E.5  E1-E.6  E1-E.7
+>=2006    E1-F.1  E1-F.2  E1-F.3  E1-F.4  E1-F.5  E1-F.6  E1-F.7
+
+For non residential buildings                           
+<0.3    0.3-0.4     0.4-0.5     0.5-0.6     0.6-0.7     0.7-0.8     >=0.8
+<=1945     En1-A.1     En1-A.2     En1-A.3     En1-A.4     En1-A.5     En1-A.6     En1-A.7
+1946-1960     En1-B.1     En1-B.2     En1-B.3     En1-B.4     En1-B.5     En1-B.6     En1-B.7
+1961-1980    En1-C.1     En1-C.2     En1-C.3     En1-C.4     En1-C.5     En1-C.6     En1-C.7
+1981-1990   En1-D.1     En1-D.2     En1-D.3     En1-D.4     En1-D.5     En1-D.6     En1-D.7
+1991-2005  En1-E.1     En1-E.2     En1-E.3     En1-E.4     En1-E.5     En1-E.6     En1-E.7
+>=2006    En1-F.1     En1-F.2     En1-F.3     En1-F.4     En1-F.5     En1-F.6     En1-F.7""
+"""
+
+def get_code(prefix, epoch, sv):
+    construction_age = ''
+    if epoch <= 1945: construction_age = 'A'          
+    elif epoch in range(1946, 1960): construction_age = 'B'          
+    elif epoch in range(1961, 1980): construction_age = 'C'          
+    elif epoch in range(1981, 1990): construction_age = 'D'          
+    elif epoch in range(1991, 2005): construction_age = 'E' 
+    else: construction_age = 'F' 
+    
+    compactness = ''
+    if sv <= 0.3: compactness = '1'
+    elif sv > 0.3 and sv <= 0.4: compactness = '2'
+    elif sv > 0.4 and sv <= 0.5: compactness = '3'
+    elif sv > 0.5 and sv <= 0.6: compactness = '4'
+    elif sv > 0.6 and sv <= 0.7: compactness = '5'
+    elif sv > 0.7 and sv <= 0.8: compactness = '6'
+    else: compactness = '7'
+    return prefix + '-' + construction_age + '.' + compactness 
+
+
+def get_code_for_residential_building(epoch, sv):
+    return get_code('E1', epoch, sv)
+
+
+def get_code_for_non_residential_building(epoch, sv):
+    return get_code('En1', epoch, sv)
+
